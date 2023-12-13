718299477315a784a8b07188ca8b767e178891b5
Ossama Tammam <ossamatammam02@gmail.com>
2023-12-13 17:23:35 +0200

    fixed the connection pipeline

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,21 @@
-const dotenv = require("dotenv");
-const mainApp = require("./app");
-const dbConnection = require("./utils/databaseConnect");
-
 // Env variables config file
-dotenv.config({ path: "../config.env" });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
+// DB connection settings
+const dbConnection = require("./utils/databaseConnect");
+dbConnection.query("SELECT * FROM temp", (err, result, fields) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+    console.log(fields);
+  }
+});
+
+// Start the main app
 const port = process.env.PORT || 3000;
-
-// dbConnection.query("SELECT * FROM temp;", (err, result, fields) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(result);
-//     console.log(fields);
-//   }
-// });
-
+const mainApp = require("./app");
 mainApp.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
