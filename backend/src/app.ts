fa8212de86744219ebf90f91508f1532c569b0c1
Ossama Tammam <ossamatammam02@gmail.com>
2023-12-14 22:56:36 +0200

    wow

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,16 @@
 import express, { Application } from "express";
+import userRouter from "./routes/userRoutes";
 
 const app: Application = express();
+
+// Puts data into req.body
+app.use(
+  express.json({
+    limit: "10kb", //Limited to 10kb
+  }),
+);
+
+// Routers
+app.use("/user", userRouter);
 
 export default app;
