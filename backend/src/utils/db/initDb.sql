3bbe5127a97a158569708651ac65a28bef36bd8b
Ossama Tammam <ossamatammam02@gmail.com>
2023-12-23 21:47:33 +0200

    new sql init

--- backend/src/utils/db/initDb.sql
+++ backend/src/utils/db/initDb.sql
@@ -1,19 +1,20 @@
 CREATE DATABASE IF NOT EXISTS car_rental_system;
 USE car_rental_system;
 CREATE TABLE IF NOT EXISTS car(
-    plate_id BIGINT NOT NULL,
+    car_id BIGINT AUTO_INCREMENT NOT NULL,
+    plate_id VARCHAR(10) UNIQUE NOT NULL,
     model VARCHAR(50) NOT NULL,
     year INT NOT NULL,
     price_per_day FLOAT NOT NULL,
     status ENUM("Active", "Out of Service", "Rented") NOT NULL,
     office_id BIGINT NOT NULL,
-    PRIMARY KEY (plate_id)
+    PRIMARY KEY (car_id)
 );
 CREATE TABLE IF NOT EXISTS car_specs(
-    plate_id BIGINT NOT NULL,
+    car_id BIGINT NOT NULL,
     color VARCHAR(20) NOT NULL,
     horse_power INT NOT NULL,
-    PRIMARY KEY (plate_id)
+    PRIMARY KEY (car_id)
 );
 CREATE TABLE IF NOT EXISTS office(
     office_id BIGINT AUTO_INCREMENT NOT NULL,
@@ -24,7 +25,7 @@ CREATE TABLE IF NOT EXISTS office(
 );
 CREATE TABLE IF NOT EXISTS reservation(
     reservation_id BIGINT AUTO_INCREMENT NOT NULL,
-    plate_id BIGINT NOT NULL,
+    car_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     office_id BIGINT NOT NULL,
     reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -49,10 +50,10 @@ CREATE TABLE IF NOT EXISTS user(
     PRIMARY KEY (user_id)
 );
 ALTER TABLE car
-ADD CONSTRAINT fk_car_office_id FOREIGN KEY (office_id) REFERENCES office(office_id);
+ADD CONSTRAINT fk_car_office_id FOREIGN KEY (office_id) REFERENCES office(office_id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE car_specs
-ADD CONSTRAINT fk_car_plate_id FOREIGN KEY (plate_id) REFERENCES car(plate_id);
+ADD CONSTRAINT fk_car_car_id FOREIGN KEY (car_id) REFERENCES car(car_id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE reservation
-ADD CONSTRAINT fk_reservation_plate_id FOREIGN KEY (plate_id) REFERENCES car(plate_id),
-    ADD CONSTRAINT fk_reservation_user_id FOREIGN KEY (user_id) REFERENCES user(user_id),
-    ADD CONSTRAINT fk_reservation_office_id FOREIGN KEY (office_id) REFERENCES office(office_id);
+ADD CONSTRAINT fk_reservation_car_id FOREIGN KEY (car_id) REFERENCES car(car_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT fk_reservation_user_id FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT fk_reservation_office_id FOREIGN KEY (office_id) REFERENCES office(office_id) ON UPDATE CASCADE ON DELETE CASCADE;
